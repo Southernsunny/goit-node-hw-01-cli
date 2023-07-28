ba74e37ddc71d37931fa3501d7eca4727c419226
Olia Medvetska <olia.medvetska1967@gmail.com>
2023-07-28 12:21:22 +0300

    add hw-1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# goit-node-hw-01-cli
- 
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+node index.js --action="list"
+![list](./screenshots/list.png)
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+![get](./screenshots/get.png)
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+![add](./screenshots/add.png)
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+ ![remove](./screenshots/remove.png)
---
+++ contacts.js
@@ -0,0 +1,66 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
+
+// Абсолютний шлях до файлу contacts.json
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+// Повертає масив контактів.
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+// Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+const getContactById = async id => {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === id);
+  return result || null;
+};
+
+// Повертає об'єкт доданого контакту.
+const addContact = async data => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  // запушили
+  contacts.push(newContact);
+  // перезаписали
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  // повернули
+  return newContact;
+};
+
+// Оновлює об'єкт контакту.
+const updateContactById = async (id, data) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
+
+// Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+const removeContact = async id => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContactById,
+  removeContact,
+};
